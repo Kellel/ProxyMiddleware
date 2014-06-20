fdbb3116b25052f1376857556f543b61561abf23
Kellen Fox <Kellen.Fox@wwu.edu>
2014-06-20 14:20:59 -0700

    Fix trailing slash works

--- ProxyMiddleware/ProxyMiddleware.py
+++ ProxyMiddleware/ProxyMiddleware.py
@@ -3,8 +3,10 @@
 # Reverse Proxy Middleware. This snippit of code sets the script_name environment variable to what is set in nginx
 # It then strips the common bits from the PATH_INFO variable
 
+from bottle import redirect, HTTPError, abort
+
 class ReverseProxied(object):
- 
+
     def __init__(self, wrap_app):
         self.wrap_app = wrap_app
         self.wrap_app = self.app = wrap_app
@@ -16,4 +18,25 @@ class ReverseProxied(object):
             path_info = environ['PATH_INFO']
             if path_info.startswith(script_name):
                 environ['PATH_INFO'] = path_info[len(script_name):]
-        return self.wrap_app(environ, start_response) 
+        return self.wrap_app(environ, start_response)
+
+class TrailingSlash(object):
+    def __init__(self, wrap_app):
+        self.wrap_app = wrap_app
+        self.wrap_app = self.app = wrap_app
+
+    def __call__(self, environ, start_response):
+        try:
+            self.app.router.match(environ)
+        except HTTPError:
+            PI = environ['PATH_INFO']
+            environ['PATH_INFO'] = PI + '/'
+            try:
+                self.app.router.match(environ)
+                start_response('301 Redirect', [('Location', environ['SCRIPT_NAME'] + environ['PATH_INFO']),])
+                return []
+            except:
+                environ['PATH_INFO'] = PI
+                pass
+        return self.wrap_app(environ, start_response)
+
