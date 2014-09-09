93f92785d0331c55bb6a61523394b0f255a47864
Kellen Fox <Kellen.Fox@wwu.edu>
2014-09-09 10:33:47 -0700

    add travis testing integration

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='ProxyMiddleware',
-      version='1.3',
+      version='1.4',
       description='A collection of usefull middleware for reverse proxying things',
       author='Kellen Fox',
       url='https://github.com/Kellel/ProxyMiddleware',
