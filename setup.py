4073ce1ff11aa984a28139bb0b1cbe183712bac0
Kellen Fox <kellen@cablespeed.com>
2015-05-24 18:54:55 -0700

    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='ProxyMiddleware',
-      version='1.5.1',
+      version='1.5.2',
       description='A collection of usefull middleware for reverse proxying things',
       author='Kellen Fox',
       url='https://github.com/Kellel/ProxyMiddleware',
